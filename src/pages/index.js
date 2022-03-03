69e76e080d275362325a5c2ff355c6afa5b7606b
harshita-6 <btbtl19010_harshita@banasthali.in>
2022-03-03 18:13:43 +0530

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
+import Hero from "../components/Hero/hero"
 import Layout from "../components/Layout/layout"
 
 const Index = () => {
   return (
     <Layout>
-      <div className="card shadow m-2 p-2 text-primary">
-        Test <div className=" m-2 p-2 btn btn-warning">OK</div>
-      </div>
+      <Hero/>
     </Layout>
   )
 }
