74ef12ecba3d2698c6492aa6137a03639ff6fac8
harshita-6 <btbtl19010_harshita@banasthali.in>
2022-03-03 12:15:37 +0530

    Update in pages folder

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React from "react"
 
-export default function Home() {
-  return <div>About Page </div>
+const About = () => {
+  return (<h1 className="text-success">About</h1>)
 }
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Index = () => {
+  return (
+    <div>
+      <div className="card shadow m-2 p-2 text-primary">Test  <div className=" m-2 p-2 btn btn-warning">OK</div></div>
+      <div className=" m-2 p-2 btn btn-warning">OK</div>
+    </div>
+  )
 }
+
+export default Index 
