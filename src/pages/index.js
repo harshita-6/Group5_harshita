4d32f393f5bd24df286baeca556d8e85e5ad7fca
harshita-6 <btbtl19010_harshita@banasthali.in>
2022-03-07 10:38:05 +0530

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Hero from "../components/Hero/hero"
+import Features from "../components/Features/features"
 import Layout from "../components/Layout/layout"
 
 const Index = () => {
   return (
     <Layout>
       <Hero/>
+      <Features/>
     </Layout>
   )
 }
